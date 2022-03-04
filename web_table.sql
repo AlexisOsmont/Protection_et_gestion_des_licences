a3bede705254d33bc51a02fff954575b2c0e5585
Fenrisfulsur <target.dummy.aux@gmail.com>
2022-03-04 11:33:45 +0100

    Modfication du script de création de la BDD

--- web_table.sql
+++ web_table.sql
@@ -1,7 +1,13 @@
+-- drop object if they already exists
+drop user if exists tomcat;
+drop database if exists web;
+
+-- create user and database
 create user tomcat identified by 'tomcatpasswd';
 create database web;
 use web;
 
+-- create table
 create table Admin (
     AdminId int primary key not null auto_increment,
     AdminUsername varchar(50),
@@ -17,7 +23,10 @@ create table Client (
 create table Software (
     SoftwareId int primary key not null auto_increment,
     SoftwareName varchar(50) not null,
-    SoftwareDesc varchar(200)    
+    SoftwareDesc varchar(200),
+    SoftwarePrice int not null,
+    SoftwarePriceMultiplier int not null,
+    SoftwareImg blob 
 );
 
 create table Licence (
@@ -33,8 +42,25 @@ create table Licence (
     references Software(SoftwareId)
 );
 
+-- grant privileges to tomcat on table
 grant all privileges on web.Admin to tomcat;
 grant all privileges on web.Client to tomcat;
 grant all privileges on web.Software to tomcat;
 grant all privileges on web.Licence to tomcat;    
 flush privileges;
+
+-- insert values
+
+-- Software
+insert into web.Software values (1, 'Logiciel de Comptabilité', 'Logiciel permettant de gérer des fiches de payes pour vos employés', 100, 1, NULL);
+insert into web.Software values (2, 'Super-ERP', 'Super logiciel de gestion de ressource pour votre entreprise', 200, 2, NULL);
+insert into web.Software values (3, 'Healt-Care.exe', 'Logiciel permettant de gérer les feuilles de soin des patients', 600, 1, NULL);
+insert into web.Software values (4, 'HeadInTheCloud', 'Solution de gestion de cloud', 150, 4, NULL);
+insert into web.Software values (5, 'BlockAndChain', 'Logiciel permettant de miner du dogecoin', 800, 1, NULL);
+
+-- Client
+insert into web.Client values (1, 'client', 'client@client.fr');
+
+-- Admin
+insert into web.Admin values (1, 'admin', 'admin@admin.fr');
+
