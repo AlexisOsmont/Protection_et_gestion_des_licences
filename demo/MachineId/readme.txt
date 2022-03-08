5eb15d13f55c255a2952bfe4817ee1abc380e4d8
kacihmd <kacixd76@gmail.com>
2022-03-08 21:23:19 +0100

    machine id

---
+++ demo/MachineId/readme.txt
@@ -0,0 +1,4 @@
+lancer executable/MachineID.exe vous donnera 
+le numero de serie de votre processeur
+un hash des addresses mac des cartes réseaux
+le numéro de série de votre carte mère
---
+++ demo/MachineId/source/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Text;
+
+#pragma warning disable CA1416 // Code valide uniquement sous Windows
+
+namespace MachineID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("--- Generating Device ID ---\n");
+
+            Console.WriteLine("--- Retrieving Processor Id Hash\n");
+            string processorIDHash = ProcessorIdHash();
+
+            Console.WriteLine("\n--- Retrieving Mac Address Hash");
+            string macAddressHash = MacAddressHash();
+
+            Console.WriteLine("\n--- Retrieving Baseboard Serial Number Hash");
+            string baseBoardHash = BaseBoardHash();
+
+
+            Console.WriteLine("\n--- Summary ---");
+            Console.WriteLine("Processor ID hash is : " + processorIDHash);
+            Console.WriteLine("Mac address(es) hash is : " + macAddressHash);
+            Console.WriteLine("Baseboard Serial Number hash is : " + baseBoardHash);
+
+            Console.WriteLine("\n\npress any key to exit the process...");
+            Console.ReadKey();
+        }
+
+        static string MacAddressHash()
+        {
+            const int MAC_ADDR_LENGTH = 12;
+
+            byte[]? mac_bytes = new byte[MAC_ADDR_LENGTH];
+            Console.WriteLine("mac_bytesL = " + mac_bytes.Length);
+                
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet || nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
+                {
+                    string tempMac = nic.GetPhysicalAddress().ToString();
+                    if (tempMac.Length == MAC_ADDR_LENGTH)
+                    {
+                        Console.WriteLine("\nName: " + nic.Name 
+                            + "\nType: " + nic.NetworkInterfaceType 
+                            + "\nMAC Address: " + nic.GetPhysicalAddress() );
+
+                        byte[] bytes = Encoding.ASCII.GetBytes(tempMac);
+                        if (mac_bytes != null && bytes != null)
+                        {
+                            mac_bytes = XorBytesArray(mac_bytes, bytes);
+                        }
+                    }
+                }
+            }
+
+            string hash = BitConverter.ToString(mac_bytes).Replace("-", "");
+
+            Console.WriteLine("\nFinal Mac Hash = " + hash);
+            return hash;
+        }
+
+        static string BaseBoardHash()
+        {
+            ManagementObjectSearcher myProcessorObject = new ManagementObjectSearcher("select * from Win32_BaseBoard");
+
+            foreach (ManagementObject item in myProcessorObject.Get())
+            {
+                // https://docs.microsoft.com/fr-fr/windows/win32/cimwin32prov/win32-baseboard
+                Console.WriteLine("\nName : " + item["Name"]);
+                Console.WriteLine("Manufacturer : " + item["Manufacturer"]);
+                Console.WriteLine("SerialNumber : " + item["SerialNumber"]);
+
+                string? serialNo = item["SerialNumber"].ToString();
+                if (serialNo != null)
+                {
+                    return serialNo;
+                }
+            }
+
+            Console.WriteLine("BaseBoard Serial Number can't be reached.");
+            return "";
+        }
+
+        static string ProcessorIdHash()
+        {
+            ManagementObjectSearcher myProcessorObject = new ManagementObjectSearcher("select * from Win32_Processor");
+
+            foreach (ManagementObject item in myProcessorObject.Get())
+            {
+                // https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-processor
+                Console.WriteLine("Name : " + item["Name"]);
+                Console.WriteLine("Processor Id : " + item["processorID"]);
+
+                string? processorId = item["processorID"].ToString();
+                if (processorId != null)
+                {
+                    return processorId;
+                }
+                
+            }
+            Console.WriteLine("Processor Informations can't be reached.");
+            return "";
+        }
+
+        static byte[]? XorBytesArray(byte[] first, byte[] second)
+        {
+            if (first == null || second == null 
+                || first.Length != second.Length)
+            {
+                return null;
+            }
+
+            byte[] xored = new byte[first.Length];
+
+            for (int i = 0; i < first.Length; ++i)
+            {
+                xored[i] = (byte)(first[i] ^ second[i]);
+            }
+            return xored;
+        }
+    }
+}
